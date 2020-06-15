be89aaf43e4e1f0b057bd7845c359f65ef5045cf
Bryony <gatehobryo@zest.ecs.vuw.ac.nz>
2020-06-15 15:02:15 +1200

    Add DisplayMapView to view package
    Extends JPanel, has a changed paintComponent().

---
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/view/DisplayMapView.java
@@ -0,0 +1,19 @@
+package nz.ac.vuw.engr301.group9mcs.view;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class DisplayMapView extends JPanel{
+
+	private static final long serialVersionUID = 3810711176953194652L;
+
+	public DisplayMapView() {
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+	}
+	
+}
