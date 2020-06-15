0630084b74fc8bc8f678d97fdceb1e4c0db3b0b8
Bryony <gatehobryo@zest.ecs.vuw.ac.nz>
2020-06-15 16:01:44 +1200

    Add checkstyle and NonNull

---
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/avionics/package-info.java
@@ -0,0 +1,5 @@
+
+@NonNullByDefault
+package nz.ac.vuw.engr301.group9mcs.avionics;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
---
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/montecarlo/package-info.java
@@ -0,0 +1,5 @@
+
+@NonNullByDefault
+package nz.ac.vuw.engr301.group9mcs.montecarlo;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
