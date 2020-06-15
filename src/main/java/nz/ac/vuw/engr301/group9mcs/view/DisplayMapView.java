1cdaa62e008edb6b534a84f5251df090faf328d4
Bryony <gatehobryo@zest.ecs.vuw.ac.nz>
2020-06-15 15:47:48 +1200

    Add MenuBar to top
    This allows user to have some control? What else do they need to do
    during this time?

--- src/main/java/nz/ac/vuw/engr301/group9mcs/view/DisplayMapView.java
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/view/DisplayMapView.java
@@ -1,19 +1,53 @@
 package nz.ac.vuw.engr301.group9mcs.view;
 
 import java.awt.Graphics;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import org.eclipse.jdt.annotation.Nullable;
 
-public class DisplayMapView extends JPanel{
+/**
+ * Display map view is for showing the position of the rocket. 
+ * Its viewing zoom and position should be automatically decided 
+ * based upon the position of the launch site and the position of 
+ * the rocket. It's purely output, the user shouldn't be able to engage 
+ * with it at all
+ * There will be a MapData interface that allows you to get a map 
+ * image for a certain area specified in longitude and latitude 
+ * (I'm thinking upper-left, lower-right corner notation, as it 
+ * allows the caller to choose aspect ratio in only two arguments). 
+ * 
+ * @author Bryony
+ *
+ */
 
-	private static final long serialVersionUID = 3810711176953194652L;
+public class DisplayMapView extends JPanel {
 
-	public DisplayMapView() {
-		
-	}
-	
-	public void paintComponent(Graphics g) {
-		
-	}
-	
+  private static final long serialVersionUID = 3810711176953194652L;
+
+  /**
+   * Set up JPanel for Displaying Map of Rocket's Progress.
+   */
+  public DisplayMapView() {
+    JMenuBar menu = new JMenuBar();
+    JMenu group9 = new JMenu("Group9");
+    JMenuItem exit = new JMenuItem("Quit Group9");
+    exit.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(@Nullable ActionEvent e) {
+        System.exit(0);
+      }
+    });
+    group9.add(exit);
+    menu.add(group9);
+    this.add(menu);
+  }
+  
+  public void paintComponent(@Nullable Graphics g) {
+    
+  }
+  
 }
