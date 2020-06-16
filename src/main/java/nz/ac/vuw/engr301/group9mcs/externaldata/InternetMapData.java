f7eace3fce0cb8e5a8c902e9e1cc01d090b1fdd3
Bailey Jewell <jewellbail@ecs.vuw.ac.nz>
2020-06-16 20:29:57 +1200

    Implemented InternetMapData.
    InternetMapData.convertCoordsToXY - This method converts the
    geographical polar coordinates to cartesian coordinates suitable
    for use in the OSM API.
    
    InternetMapData.get - This method perform the actual request to
    retrieve the image of the specified location.

--- src/main/java/nz/ac/vuw/engr301/group9mcs/externaldata/InternetMapData.java
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/externaldata/InternetMapData.java
@@ -1,15 +1,18 @@
 package nz.ac.vuw.engr301.group9mcs.externaldata;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.eclipse.jdt.annotation.Nullable;
-
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
 
 public class InternetMapData {
 
-    @Nullable
-    HttpClient httpClient;
+    /**
+     * This is the basic format for the OSM tile URI.
+     *
+     * <br>The endpoint "a" is specified here, but two others ("b" and "c") are also provided.
+     */
+    private static final String osmTileUriFormat = "http://a.tile.openstreetmap.org/%d/%d/%d.png";
 
     /**
      * Gets an image from the OpenStreetMap API.
@@ -21,9 +24,14 @@ public class InternetMapData {
      * @return Returns an Image object of the specified location.
      */
     public Image get(double latitude, double longitude, int zoom) {
-        double[] location = convertCoordsToXY(latitude, longitude, zoom);
-        setupHttpClient();
-        return null;
+        int[] location = convertCoordsToXY(latitude, longitude, zoom);
+        try {
+            return ImageIO.read(new URL(String.format(osmTileUriFormat, zoom, location[0], location[1])));
+        } catch (IOException e) {
+            e.printStackTrace();
+            // TODO: Determine error handling.
+            return null;
+        }
     }
 
     /**
@@ -36,9 +44,9 @@ public class InternetMapData {
      * @param latitude Latitude (degrees) of centre point of image to be requested.
      * @param longitude Longitude (degrees) of centre point of image to be requested.
      * @param zoom Zoom level. Is used as a scalar when calculating XY coordinates.
-     * @return Returns a double array containing the X and Y coordinates of the given latitude and longitude.
+     * @return Returns an integer array containing the X and Y coordinates of the given latitude and longitude.
      */
-    private static double[] convertCoordsToXY(double latitude, double longitude, int zoom) {
+    private static int[] convertCoordsToXY(double latitude, double longitude, int zoom) {
         // Converting coordiantes to radians for use in tile formula.
         double latitudeRadians = Math.toRadians(latitude);
         double longitudeRadians = Math.toRadians(longitude);
@@ -46,14 +54,6 @@ public class InternetMapData {
         int n = (int) Math.pow(2, zoom);
         int x = (int) (n * ((longitude + 180f) / 380f));
         int y = (int) (n * (1 - (Math.log(Math.tan(latitudeRadians) + 1 / Math.cos(latitudeRadians)) / Math.PI)) / 2);
-        return new double[]{x, y};
-    }
-
-    /**
-     * Creates a HttpClientBuilder and builds a new default HttpClient.
-     */
-    private void setupHttpClient() {
-        HttpClientBuilder builder = HttpClientBuilder.create();
-        httpClient = builder.build();
+        return new int[]{x, y};
     }
 }
