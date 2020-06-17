1955fa5a78e229b7af936e4fd85efbab1b331dfb
Joshua Hindley <hindlejosh@ecs.vuw.ac.nz>
2020-06-17 13:21:11 +1200

    Added a placeholder method to get an image from the InternetMapData This version of this class will be deleted and will be replaced by the version written by @author Bailey Jewell.

--- src/main/java/nz/ac/vuw/engr301/group9mcs/externaldata/InternetMapData.java
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/externaldata/InternetMapData.java
@@ -1,11 +1,27 @@
 package nz.ac.vuw.engr301.group9mcs.externaldata;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
 /**
  * A placeholder InternetMapData class.
  * Will be replaced by the actual class written by @author Bailey Jewell.
  */
 public class InternetMapData {
-  
+
   public InternetMapData() { }
-  
+
+  /**
+   * This is a placeholder.
+   * @param latitude lat
+   * @param longitude long
+   * @param zoom zoom
+   * @return
+   */
+  public static Image get(double latitude, double longitude, double zoom) {
+    if (latitude > 0 && longitude > 0 && zoom > 0) {
+      return new BufferedImage(0, 0, 0);
+    }
+    return new BufferedImage(0, 0, 0);
+  }
 }
