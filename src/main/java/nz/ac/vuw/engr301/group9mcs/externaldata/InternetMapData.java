07ee1b189d9e076abc72f12bb6ceb3176012e42b
Joshua Hindley <hindlejosh@ecs.vuw.ac.nz>
2020-06-17 13:00:04 +1200

    Added placeholder InternetMapData class

---
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/externaldata/InternetMapData.java
@@ -0,0 +1,11 @@
+package nz.ac.vuw.engr301.group9mcs.externaldata;
+
+/**
+ * A placeholder InternetMapData class.
+ * Will be replaced by the actual class written by @author Bailey Jewell.
+ */
+public class InternetMapData {
+  
+  public InternetMapData() { }
+  
+}
