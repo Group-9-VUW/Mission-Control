9a24e4fafb5880a6da97efbe79a3353bdb6120bb
Joshua Hindley <hindlejosh@ecs.vuw.ac.nz>
2020-06-18 17:13:08 +1200

    Removed unused method

--- src/main/java/nz/ac/vuw/engr301/group9mcs/externaldata/MapData.java
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/externaldata/MapData.java
@@ -11,30 +11,6 @@ package nz.ac.vuw.engr301.group9mcs.externaldata;
  *
  */
 public interface MapData {
-  
-  /**
-   * Converts the latitude and longitude coordinates (degrees) to Cartesian tile coordinates using
-   * <a href=https://en.wikipedia.org/wiki/Mercator_projection>Mercator Projection</a>.
-   *
-   * <br>This calculation is made from the formula available at the
-   * <a href=https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Derivation_of_tile_names>OSM Wiki</a>.
-   *
-   * @param latitude Latitude (degrees) of centre point of image to be requested.
-   * @param longitude Longitude (degrees) of centre point of image to be requested.
-   * @param zoom Zoom level. Is used as a scalar when calculating XY coordinates.
-   * @return An int array containing the X and Y coordinates of the given latitude and longitude.
-   * 
-   * @author Bailey Jewell, (jewellbail)
-   */
-  static int[] convertCoordsToXY(double latitude, double longitude, int zoom) {
-    // Converting latitude to radians for use in tile formula.
-    double latitudeRadians = Math.toRadians(latitude);
+  //No methods required at the moment.
 
-    // Calculating cartesian coordinates.
-    int n = (int) Math.pow(2, zoom);
-    int x = (int) (n * ((longitude + 180.0) / 360.0));
-    int y = (int) (n * (1 - (Math.log(Math.tan(latitudeRadians) + 1 
-        / Math.cos(latitudeRadians)) / Math.PI)) / 2);
-    return new int[]{x, y};
-  }
 }
