94a32ce9dd7d14d37190ff457c8b55c172a8f986
Claire <foxcoindev@gmail.com>
2020-06-18 12:11:11 +1200

    Added basic JFrame child

---
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/controller/MapSpike.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package nz.ac.vuw.engr301.group9mcs.controller;
+
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+/**
+ * A runner class for the map demo. Executable.
+ * <br><br>
+ * Implements the main JFrame
+ * 
+ * @author Claire
+ */
+public class MapSpike extends JFrame {
+
+	private static final long serialVersionUID = 8869451586408924734L;
+
+	/**
+	 * @throws HeadlessException
+	 */
+	public MapSpike() throws HeadlessException {
+		super("Map Demo");
+	}
+
+}
