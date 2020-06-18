c807af22d56840aa2541f0b2076699e443174f9c
Claire Chambers <chambeconn@ecs.vuw.ac.nz>
2020-06-18 13:21:31 +1200

    Added SimpleEventListener

---
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/commons/SimpleEventListener.java
@@ -0,0 +1,12 @@
+package nz.ac.vuw.engr301.group9mcs.commons;
+
+/**
+ * A simplistic event listener that can be used anywhere
+ * 
+ * @author Claire
+ */
+public interface SimpleEventListener {
+	
+	void event(String type);
+
+}
