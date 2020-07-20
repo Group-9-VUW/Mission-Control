44f36680c07f305466cc0f1775b511b843051145
hindlejosh <hindlejosh@petit-lyon.ecs.vuw.ac.nz>
2020-07-21 11:24:23 +1200

    Removed warnings from SelectMapView class There is a warning that cannot be removed, but this warning should never result in an error being thrown.

--- src/main/java/nz/ac/vuw/engr301/group9mcs/view/SelectMapView.java
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/view/SelectMapView.java
@@ -16,15 +16,17 @@ import javax.swing.JPanel;
 import org.eclipse.jdt.annotation.Nullable;
 
 import nz.ac.vuw.engr301.group9mcs.commons.LongLatHelper;
+import nz.ac.vuw.engr301.group9mcs.controller.SelectDemoPanel;
 import nz.ac.vuw.engr301.group9mcs.externaldata.MapData;
 
-/** 
+/**
  * This view displays the Map in a specified window. The user can zoom in/out of the map using a mouse scroll wheel
- * They can also move the map via dragging and if they click on a spot it will highlight that spot and ask if they 
+ * They can also move the map via dragging and if they click on a spot it will highlight that spot and ask if they
  * would like to choose the location as the launch location.
- * 
+ *
  * @author August
  * @editor Claire
+ * @formatter Joshua
  */
 public class SelectMapView extends JPanel implements MouseListener, MouseMotionListener, MouseWheelListener, ComponentListener {
 
@@ -32,7 +34,7 @@ public class SelectMapView extends JPanel implements MouseListener, MouseMotionL
 
 	private int sizeX; //Total size (in x pixels) of the map
 	private int sizeY; //Total size (in y pixels) of the map
-	private Image image; //Current image (current map) being displayed
+	private @Nullable Image image; //Current image (current map) being displayed
 	private double pixelToLat; //How many latitudes in a pixel
 	private double pixelToLon; //How many longitudes in a pixel
 	private int initialX; //Initial (first) x pixel of mouse drag
@@ -42,162 +44,161 @@ public class SelectMapView extends JPanel implements MouseListener, MouseMotionL
 	private int locSize; //Size of highlighted location
 	private boolean locationSelected; //Has the user selected a location (launch site)
 	private MapData mapData; //Used to get the map images
-	private LaunchSelectedListener launchListener;
-	
+	private @Nullable LaunchSelectedListener launchListener;
+
 	private double lat = -41.291257;
 	private double lon = 174.776879;
-	
+
 	/**
 	 * As the viewport won't be a perfect square, this is the 'radius' for the Y axis,
-	 * ie the distance from the screen's center to the top and bottom of the screen
+	 * i.e. the distance from the screen's center to the top and bottom of the screen.
 	 */
 	private double radY = 0.5;
-	
-	/** This sets up fields, draws the initial map and determines longitude/latitude to pixel ratios */
-	public SelectMapView(MapData data) {
-		mapData = data;
-		locationSelected = false;
-		locSize = 20;
-		sizeX = this.getWidth();
-		sizeY = this.getHeight();
+
+	/**
+	 * This sets up fields, draws the initial map and determines longitude/latitude to pixel ratios
+	 */
+	public SelectMapView(final MapData data) {
+		this.mapData = data;
+		this.locationSelected = false;
+		this.locSize = 20;
+		this.sizeX = this.getWidth();
+		this.sizeY = this.getHeight();
 		this.addMouseListener(this);
 		this.addMouseMotionListener(this);
 		this.addMouseWheelListener(this);
 		this.addComponentListener(this);
 	}
-	
+
 	protected double getRadY()
 	{
 		return this.radY;
 	}
-	
+
 	protected double getRadX()
 	{
-		return (radY / sizeY) * sizeX;
+		return (this.radY / this.sizeY) * this.sizeX;
 	}
-	
+
 	protected double getUpperLat()
 	{
-		return LongLatHelper.latitudeNKilometersNorth(lat, this.getRadY());
+		return LongLatHelper.latitudeNKilometersNorth(this.lat, this.getRadY());
 	}
-	
+
 	protected double getLowerLat()
 	{
-		return LongLatHelper.latitudeNKilometersSouth(lat, this.getRadY());
+		return LongLatHelper.latitudeNKilometersSouth(this.lat, this.getRadY());
 	}
-	
+
 	protected double getLeftLon()
 	{
-		return LongLatHelper.longditudeNKilometersWest(lat, lon, this.getRadX());
+		return LongLatHelper.longditudeNKilometersWest(this.lat, this.lon, this.getRadX());
 	}
-	
+
 	protected double getRightLon()
 	{
-		return LongLatHelper.longditudeNKilometersEast(lat, lon, this.getRadX());
+		return LongLatHelper.longditudeNKilometersEast(this.lat, this.lon, this.getRadX());
 	}
-	
-	/** 
-	 * Calculates how many longitudes/latitudes are in a pixel 
+
+	/**
+	 * Calculates how many longitudes/latitudes are in a pixel
 	 */
-	private void pixelToLongLat() 
+	private void pixelToLongLat()
 	{
-		pixelToLon = (2 * (this.getRadX() / LongLatHelper.kilometeresPerDegreeOfLongitude(lat))) / sizeX;
-		pixelToLat = (2 * (this.getRadY() / LongLatHelper.kilometersPerDegreeOfLatitude(lat))) / sizeY;
+		this.pixelToLon = (2 * (this.getRadX() / LongLatHelper.kilometeresPerDegreeOfLongitude(this.lat))) / this.sizeX;
+		this.pixelToLat = (2 * (this.getRadY() / LongLatHelper.kilometersPerDegreeOfLatitude(this.lat))) / this.sizeY;
 	}
-	
+
 	@Override
 	public void paintComponent(@Nullable Graphics g) {
 		if (g == null) { return; }
-		image = mapData.get(this.getUpperLat(), this.getLeftLon(), this.getLowerLat(), this.getRightLon()); //Gets the image
+		this.image = this.mapData.get(this.getUpperLat(), this.getLeftLon(), this.getLowerLat(), this.getRightLon()); //Gets the image
 		//g.drawImage(image, 0, 0, this); //Draws it
-		g.drawImage(image, 0, 0, sizeX, sizeY, this);
-		if (locationSelected) { //If location has been selected then still draw it
-			this.getGraphics().fillOval(locationX-(locSize/2), locationY-(locSize/2), locSize, locSize);
-		}		
-	}
-
-	@Override
-	public void mouseDragged(MouseEvent arg0) {
-		int currentX = arg0.getX(); 
-		int currentY = arg0.getY();
-		int diffX = currentX - initialX;
-		int diffY = currentY - initialY;
-		/* Converting pixel difference to longitude/latitude difference and adding/subtracting that from current longitude/latitude */
-		lon = lon - diffX * pixelToLon;
-		lat = lat + diffY * pixelToLat;
-		
-		repaint(); //Draw the new map
-		
-		/* Moving the highlighted location */
-		locationX = locationX + diffX;
-		locationY = locationY + diffY;
-		
-		initialX = currentX;
-		initialY = currentY;
-	}
-
-	@Override
-	public void mouseMoved(MouseEvent arg0) {
-		
-	}
-
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		/* Asks user if they want to select the location as their launch site in a pop-up window */
-		int reply = JOptionPane.showConfirmDialog(this, "Would you like to choose this location as your launch location?"); //Asking if they would like to confirm it
-		if (reply == JOptionPane.YES_OPTION) { //If they do 
-			locationX = e.getX();
-			locationY = e.getY();
-			this.getGraphics().setColor(Color.RED); //Then location will be marked red
-			this.getGraphics().fillOval(locationX-(locSize/2), locationY-(locSize/2), locSize, locSize); //Marking the location
-			if (locationSelected) {
-				locationSelected = false;
-			}
-			else {
-				locationSelected = true;
-			}
-			launchListener.onLaunchSelected((this.getUpperLat() - (locationY * pixelToLat)), (this.getLeftLon() + (locationX * pixelToLon)));
+		g.drawImage(this.image, 0, 0, this.sizeX, this.sizeY, this);
+		if (this.locationSelected) { //If location has been selected then still draw it
+			this.getGraphics().fillOval(this.locationX-(this.locSize/2), this.locationY-(this.locSize/2), this.locSize, this.locSize);
 		}
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent e) {
+	public void mouseDragged(@Nullable MouseEvent arg0) {
+		if (arg0 == null) {return;}
+		int currentX = arg0.getX();
+		int currentY = arg0.getY();
+		int diffX = currentX - this.initialX;
+		int diffY = currentY - this.initialY;
+		/* Converting pixel difference to longitude/latitude difference and adding/subtracting that from current longitude/latitude */
+		this.lon = this.lon - diffX * this.pixelToLon;
+		this.lat = this.lat + diffY * this.pixelToLat;
+
+		repaint(); //Draw the new map
+
+		/* Moving the highlighted location */
+		this.locationX = this.locationX + diffX;
+		this.locationY = this.locationY + diffY;
+
+		this.initialX = currentX;
+		this.initialY = currentY;
+	}
+
+	@Override
+	public void mouseMoved(@Nullable MouseEvent arg0) {
+		//This method must be overridden, but is not used.
+	}
+
+	@Override
+	public void mouseClicked(@Nullable MouseEvent e) {
+		/* Asks user if they want to select the location as their launch site in a pop-up window */
+		if (e == null || this.launchListener == null) {return;}
+		int reply = JOptionPane.showConfirmDialog(this, "Would you like to choose this location as your launch location?"); //Asking if they would like to confirm it
+		if (reply == JOptionPane.YES_OPTION) { //If they do
+			this.locationX = e.getX();
+			this.locationY = e.getY();
+			this.getGraphics().setColor(Color.RED); //Then location will be marked red
+			this.getGraphics().fillOval(this.locationX-(this.locSize/2), this.locationY-(this.locSize/2), this.locSize, this.locSize); //Marking the location
+			this.locationSelected = !this.locationSelected;
+			this.launchListener.onLaunchSelected((this.getUpperLat() - (this.locationY * this.pixelToLat)), (this.getLeftLon() + (this.locationX * this.pixelToLon)));
+		}
+	}
+
+	@Override
+	public void mouseEntered(@Nullable MouseEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
-	public void mouseExited(MouseEvent e) {
+	public void mouseExited(@Nullable MouseEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
-	public void mousePressed(MouseEvent e) {
-		initialX = e.getX();
-		initialY = e.getY();
+	public void mousePressed(@Nullable MouseEvent e) {
+		//TODO add condition for only certain mouse press events
+		if (e == null) {return;}
+		this.initialX = e.getX();
+		this.initialY = e.getY();
 	}
 
 	@Override
-	public void mouseReleased(MouseEvent e) {
+	public void mouseReleased(@Nullable MouseEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
-	public void mouseWheelMoved(MouseWheelEvent arg0) {
+	public void mouseWheelMoved(@Nullable MouseWheelEvent arg0) {
+		if (arg0 == null) {return;}
 		int notches = arg0.getWheelRotation(); //Gets how much the wheel has rotated
 		this.radY = this.radY * (1.0 + (notches * 0.1));
 		this.pixelToLongLat();
 		repaint(); //Repaint (show new) map
 	}
-	
+
 	public void addListener(LaunchSelectedListener listener) {
-		launchListener = listener;
+		this.launchListener = listener;
 	}
 
 	@Override
-	public void componentResized(ComponentEvent e) {
+	public void componentResized(@Nullable ComponentEvent e) {
 		this.sizeX = this.getSize().width;
 		this.sizeY = this.getSize().height;
 		this.pixelToLongLat();
@@ -205,21 +206,18 @@ public class SelectMapView extends JPanel implements MouseListener, MouseMotionL
 	}
 
 	@Override
-	public void componentMoved(ComponentEvent e) {
+	public void componentMoved(@Nullable ComponentEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
-	public void componentShown(ComponentEvent e) {
+	public void componentShown(@Nullable ComponentEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
-	public void componentHidden(ComponentEvent e) {
+	public void componentHidden(@Nullable ComponentEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 }
 
