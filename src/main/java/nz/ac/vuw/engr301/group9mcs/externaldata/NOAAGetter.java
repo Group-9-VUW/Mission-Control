4d5030a2bc7167a6c3c348cd2c537977bae0bd56
hindlejosh <hindlejosh@petit-lyon.ecs.vuw.ac.nz>
2020-07-21 11:41:53 +1200

    Removed warnings for the NOAAGetter class

--- src/main/java/nz/ac/vuw/engr301/group9mcs/externaldata/NOAAGetter.java
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/externaldata/NOAAGetter.java
@@ -13,20 +13,20 @@ import org.json.JSONObject;
 import nz.ac.vuw.engr301.group9mcs.commons.WeatherData;
 
 /**
- * This class connects to the OpenWeatherMap one call API and retrieves weather data from it. 
- * The weather data is returned in the JSON format. 
- * The JSON will be parsed and the info will be pushed to all other packages that need it. 
- * 
- * 
+ * This class connects to the OpenWeatherMap one call API and retrieves weather data from it.
+ * The weather data is returned in the JSON format.
+ * The JSON will be parsed and the info will be pushed to all other packages that need it.
+ *
+ *
  * @author Sai Panda, pandasai, 300449188
  */
 public class NOAAGetter {
 	/*
-	 * The appid for the OpenWeatherMap API. The user will supply their own token that will be used. 
+	 * The appid for the OpenWeatherMap API. The user will supply their own token that will be used.
 	 */
 	private String appid = "ead647e24776f26ed6f63af5f1bbf68c";
-	
-	
+
+
 	/**
 	 * Default constuctor for testing.
 	 */
@@ -38,15 +38,15 @@ public class NOAAGetter {
 			scan.close();
 		}
 	}
-	
+
 	/**
 	 * Constructor for when the user supplies their API token.
-	 * @param token the token the user has supplied. 
+	 * @param token the token the user has supplied.
 	 */
 	public NOAAGetter(String appid) {
 		this.appid = appid;
 	}
-	
+
 	/**
 	 * Gets the current weather at the supplied latitude and longitude.
 	 * @param latitude - latitude of the location.
@@ -58,34 +58,34 @@ public class NOAAGetter {
 			String units = "metric";
 			String urlString = "https://api.openweathermap.org/data/2.5/onecall?"
 					+ "lat=" + latitude + "&lon=" + longitude + "&units=" + units + "&exclude=daily,hourly,minutely&appid=" + this.appid;
-			
+
 			URL url = new URL(urlString);
 			URLConnection connection = url.openConnection();
-			
+
 			try(BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));){
-			
+
 				JSONObject weatherJSON = new JSONObject(reader.readLine());
-				
+
 				JSONObject currentData = weatherJSON.getJSONObject("current");
-				
+
                 JSONObject rainData = null;
-                
+
                 if(currentData.has("rain")) {
                     rainData = currentData.getJSONObject("rain");
                 }
-				
-				
+
+
 				double temperature = currentData.getDouble("temp");
-				
-				// The units for the wind speed returned by the API is in meters per second. 
-				// So we need to convert it to kilometers per hour as that is the standard unit of measurement for wind in New Zealand. 
+
+				// The units for the wind speed returned by the API is in meters per second.
+				// So we need to convert it to kilometers per hour as that is the standard unit of measurement for wind in New Zealand.
 				double windSpeed = ((currentData.getDouble("wind_speed") * 60) * 60) / 1000;
-				
+
 				double pressure = currentData.getDouble("pressure");
-				
+
                 // Amount of Rainfall in the last hour.
                 double precipitation = rainData != null && rainData.keySet().contains("1h") == true ? rainData.getDouble("1h") : 0.0;
-				
+
 				reader.close();
 				return new WeatherData(temperature, windSpeed, pressure, precipitation);
 			}
@@ -93,27 +93,27 @@ public class NOAAGetter {
 			// TODO Auto-generated catch block.
 			e.printStackTrace();
 		}
-		return null;
+		return new WeatherData(0, 0, 0, 0); //TODO change this
 	}
 
-	
+
 	/**
 	 * @return the appid.
 	 */
 	public String getAppId() {
 		return this.appid;
 	}
-	
+
 	/**
 	 * @param appid the appid to set.
 	 */
 	public void setAppId(String token) {
 		this.appid = token;
 	}
-	
+
 	/**
-	 * Checks if the user can succesfully connect to the OpenWeatherMap API. 
-	 * @return true if the user can connect to the API, false otherwise.  
+	 * Checks if the user can succesfully connect to the OpenWeatherMap API.
+	 * @return true if the user can connect to the API, false otherwise.
 	 */
 	public static boolean isAvailable() {
 		try {
@@ -121,25 +121,25 @@ public class NOAAGetter {
 	         URLConnection connection = url.openConnection();
 	         connection.connect();
 	      } catch (MalformedURLException e) {
-	         return false; 
+	         return false;
 	      } catch (IOException e) {
-	         return false; 
+	         return false;
 	      }
-		return true;	
+		return true;
 	}
 
 
 	/**
-	 * Test main method. 
+	 * Test main method.
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		System.out.println("Connection to OWM: " + (NOAAGetter.isAvailable() == true ? "Successful" : "Failed"));
 		System.out.println();
-		
+
 		NOAAGetter getter = new NOAAGetter("ead647e24776f26ed6f63af5f1bbf68c");
 		System.out.println(getter.getWeatherData(-41.289224, 174.768352));
-		
+
 	}
-	
+
 }
