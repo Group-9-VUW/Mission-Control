3d3647c89add6582333c9ae19d9fe2cdcad486be
Bryony Gatehouse <gatehobryo@ecs.vuw.ac.nz>
2020-07-24 09:44:58 +1200

    Create View Controller
    Chooses the correct view based on information from controller ->
    Controller shouldn't know what is inside the View Package!

---
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/view/ViewController.java
@@ -0,0 +1,19 @@
+package main.java.nz.ac.vuw.engr301.group9mcs.view;
+
+import javax.swing.JPanel;
+
+/**
+ * @author Bryony
+ *
+ */
+public class ViewController{
+
+	/**
+	 * @param panel
+	 * @return Returns the current view
+	 */
+	public static JPanel getCurrentView(int panel) {
+		return new JPanel();
+	}
+	
+}
