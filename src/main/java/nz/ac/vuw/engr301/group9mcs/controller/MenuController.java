89b52d7d4b56aa196e17d9b712fa978d95d1c66a
Bryony Gatehouse <gatehobryo@ecs.vuw.ac.nz>
2020-07-24 10:20:48 +1200

    Add extra methods to Menu Controller
    These were deleted somehow??? Basically methods to manipulate the menus
    outside of the class.

--- src/main/java/nz/ac/vuw/engr301/group9mcs/controller/MenuController.java
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/controller/MenuController.java
@@ -53,41 +53,12 @@ public class MenuController {
 		file.add(quit);
 		
 		this.selectLaunch = new JMenu("Select Launch");
-		JMenuItem enter = new JMenuItem("Enter Coordinates");
-		enter.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(@Nullable ActionEvent e) {
-				// TODO What to do?
-			}
-		});
-		this.selectLaunch.add(enter);
 		this.selectLaunch.setEnabled(false);
 		
 		this.preLaunch = new JMenu("Pre-Launch");
-		JMenuItem enterPre = new JMenuItem("Enter Coordinates");
-		enterPre.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(@Nullable ActionEvent e) {
-				// TODO What to do?
-			}
-		});
-		this.preLaunch.add(enterPre);
 		this.preLaunch.setEnabled(false);
 		
 		this.launch = new JMenu("Launch");
-		JMenuItem labels = new JMenuItem("Hide Coordinates");
-		labels.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(@Nullable ActionEvent e) {
-				// TODO Auto-generated method stub
-				if(labels.getText().equals("Hide Coordinates")){
-					labels.setText("See Coordinates");
-				} else {
-					labels.setText("Hide Coordinates");
-				}
-			}
-		});
-		this.launch.add(labels);
 		this.launch.setEnabled(false);
 		
 		menu.add(file);
@@ -95,6 +66,64 @@ public class MenuController {
 		menu.add(this.preLaunch);
 		menu.add(this.selectLaunch);
 		return menu;
+	}
+	
+	/**
+	 * @param name
+	 * @return Returns the JMenu that matches the name (launch is default)
+	 */
+	private JMenu getMenuFromString(String name) {
+		if((name.toLowerCase()).contains("select")) {
+			return this.selectLaunch;
+		} else if ((name.toLowerCase()).contains("pre")) {
+			return this.preLaunch;
+		} else {
+			return this.launch;
+		}
+	}
+	
+	/**
+	 * @param name 
+	 * @param items
+	 * @param total
+	 * @return Returns true if the total is smaller or equal to item.length
+	 */
+	public boolean setMenuItems(String name, JMenuItem[] items, int total) {
+		if(items.length > total) {
+			return false;
+		}
+		JMenu menu = getMenuFromString(name);
+		menu.removeAll();
+		for(int i = 0; i < total; i++) {
+			menu.add(items[i]);
+		}
+		return true;
+	}
+	
+	/**
+	 * @param name 
+	 * @return Returns true if there are already Menu Items in the Menu
+	 */
+	public boolean menuContainsOptions(String name) {
+		return getMenuFromString(name).getMenuComponentCount() > 0;
+	}
+	
+	/**
+	 * @param name
+	 */
+	public void enableMenu(String name) {
+		disableMenu("pre");
+		disableMenu("select");
+		disableMenu("");
+		
+		getMenuFromString(name).setEnabled(true);
+	}
+	
+	/**
+	 * @param name
+	 */
+	public void disableMenu(String name) {
+		getMenuFromString(name).setEnabled(false);
 	}
 	
 }
