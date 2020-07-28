a8a7bed6924a69d67896d82938590540dad3d6ac
Bryony Gatehouse <gatehobryo@ecs.vuw.ac.nz>
2020-07-28 12:33:40 +1200

    Change FakePerspective to use updated MenuController
    Can now add name and actionlistener.

--- src/test/java/test/nz/ac/vuw/engr301/group9mcs/controller/FakePerspective.java
+++ src/test/java/test/nz/ac/vuw/engr301/group9mcs/controller/FakePerspective.java
@@ -2,28 +2,33 @@ package test.nz.ac.vuw.engr301.group9mcs.controller;
 
 import java.awt.event.ActionListener;
 import java.util.Map;
+import java.util.Observable;
 import java.util.Observer;
 import javax.swing.JPanel;
 import nz.ac.vuw.engr301.group9mcs.controller.MenuController;
 import nz.ac.vuw.engr301.group9mcs.controller.Perspective;
 
-public class FakePerspective implements Perspective{
+public class FakePerspective extends Observable implements Perspective{
 
 	private Map<String, ActionListener> menuItems;
+	private JPanel panel;
 
 	@Override
 	public JPanel enable(MenuController menu) {
-		// TODO Auto-generated method stub
-		return null;
+		menu.enableItems((String[])this.menuItems.keySet().toArray());
+		return this.panel;
 	}
 
 	@Override
 	public void init(MenuController menu, Observer o) {
-
+		for(String s : this.menuItems.keySet()) {
+			menu.addMenuItem(s, this.menuItems.get(s));
+		}
+		this.addObserver(o);
 	}
 
 	public void add(String name, ActionListener a) {
-		menuItems.put(name, a);
+		this.menuItems.put(name, a);
 	}
 
 }
