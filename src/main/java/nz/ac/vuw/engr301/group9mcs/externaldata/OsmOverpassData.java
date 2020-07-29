9083d6496224bd235b56b0a9538bbadac5774c76
Bailey Jewell <baileyjjewell@gmail.com>
2020-07-29 15:25:18 +1200

    Defined data structure for Overpass responses.

--- src/main/java/nz/ac/vuw/engr301/group9mcs/externaldata/OsmOverpassData.java
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/externaldata/OsmOverpassData.java
@@ -1,38 +1,43 @@
 package nz.ac.vuw.engr301.group9mcs.externaldata;
 
 /**
- * This class serves to retrieve pure map data for a specified region, including nodes, ways and the associated
- * metadata from OpenStreetMap Overpass.
+ * This class represents a set of data retrieved from the OSM Overpass API.
  *
- * @author Bailey Jewell (jewellbail)
+ * @author Bailey Jewell
  */
 public class OsmOverpassData {
 
-    /**
-     * Determines whether or not a given location is on a forbidden area.
-     *
-     * Forbidden areas include:
-     *  - Buildings
-     *  - Forests
-     *
-     * @param latitude Latitude of point.
-     * @param longitude Longitude of point.
-     * @return Returns true if on a building, false otherwise.
-     */
-    public static boolean isOnForbiddenArea(double latitude, double longitude) {
-        // TODO: Implement me!
-        return false;
+    final Node[] nodes;
+    final Way[] ways;
+
+    public OsmOverpassData(Node[] nodes, Way[] ways) {
+        this.nodes = nodes;
+        this.ways = ways;
     }
 
     /**
-     * Gets the building data around a point.
-     *
-     * @param latitude Latitude of point.
-     * @param longitude Longitude of point.
-     * @return Returns the JSON response from Overpass.
+     * Represents a single OpenStreetMap node. Every OSM entity comprises nodes.
      */
-    private static String getAreasAtPoint(double radius, double latitude, double longitude) {
-        // TODO: Implement me!
-        return null;
+    static final class Node {
+        final int id;
+        final double lat;
+        final double lon;
+
+        public Node(int id, double lat, double lon) {
+            this.id = id;
+            this.lat = lat;
+            this.lon = lon;
+        }
+    }
+
+    /**
+     * Represents a single OpenStreetMap way. Ways comprise nodes.
+     */
+    static final class Way {
+        final Node[] nodes;
+
+        public Way(Node[] nodes) {
+            this.nodes = nodes;
+        }
     }
 }
