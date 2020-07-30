1d779efb547e32e5b3721c189f4a4a669fb529e7
Bryony Gatehouse <gatehobryo@ecs.vuw.ac.nz>
2020-07-31 11:03:35 +1200

    Edit Fake Perspective
    Add JavaDocs, Add NonNull/Nullable, Add disableAll (current menu doesn't
    disable all menuitems before enabling the next list).
    
    Extend constuctor to have name and panel parameters. If panel isn't
    null, it's saved as the perspective's panel. The Panel's name is set to
    the passed name.

--- src/test/java/test/nz/ac/vuw/engr301/group9mcs/controller/FakePerspective.java
+++ src/test/java/test/nz/ac/vuw/engr301/group9mcs/controller/FakePerspective.java
@@ -6,33 +6,62 @@ import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
 import javax.swing.JPanel;
+
+import org.eclipse.jdt.annotation.NonNull;
+
 import nz.ac.vuw.engr301.group9mcs.controller.MenuController;
 import nz.ac.vuw.engr301.group9mcs.controller.Perspective;
 
+/**
+ * @author Bryony
+ *
+ */
 public class FakePerspective extends Observable implements Perspective{
 
+	/**
+	 * 
+	 */
 	private Map<String, ActionListener> menuItems;
+	/**
+	 * 
+	 */
 	private JPanel panel = new JPanel();
 
-	public FakePerspective() {
+	/**
+	 * @param name 
+	 * @param panel 
+	 * 
+	 */
+	public FakePerspective(String name, JPanel panel) {
 		this.menuItems = new HashMap<>();
+		if(panel != null) {
+			this.panel = panel;
+		}
+		this.panel.setName(name);
 	}
 
+	@SuppressWarnings("null")
 	@Override
-	public JPanel enable(MenuController menu) {
+	public JPanel enable(@NonNull MenuController menu) {
 		String[] a = new String[this.menuItems.size()];
+		menu.disableAll();
 		menu.enableItems(this.menuItems.keySet().toArray(a));
 		return this.panel;
 	}
 
+	@SuppressWarnings("null")
 	@Override
-	public void init(MenuController menu, Observer o) {
+	public void init(@NonNull MenuController menu, @NonNull Observer o) {
 		for(String s : this.menuItems.keySet()) {
 			menu.addMenuItem(s, this.menuItems.get(s));
 		}
 		this.addObserver(o);
 	}
 
+	/**
+	 * @param name
+	 * @param a
+	 */
 	public void add(String name, ActionListener a) {
 		this.menuItems.put(name, a);
 	}
