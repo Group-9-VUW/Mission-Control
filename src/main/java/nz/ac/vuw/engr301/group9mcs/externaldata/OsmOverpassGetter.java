4cc34eeafede899096a71e7de5a446c8e5c2d953
Bailey Jewell <baileyjjewell@gmail.com>
2020-07-30 21:45:37 +1200

    Initial basic implementation of getAreasAtPoint().
    At this stage, we make the request and simply return the JSON string.
    This will be parsed into the appropriate objects in the future.
    
    Additionally, this query is for a single point. It will probably be
    better to retrieve the map data for a larger area here.

--- src/main/java/nz/ac/vuw/engr301/group9mcs/externaldata/OsmOverpassGetter.java
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/externaldata/OsmOverpassGetter.java
@@ -1,5 +1,9 @@
 package nz.ac.vuw.engr301.group9mcs.externaldata;
 
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+
 /**
  * This class serves to retrieve pure map data for a specified region, including nodes, ways and the associated
  * metadata from OpenStreetMap Overpass.
@@ -7,6 +11,20 @@ package nz.ac.vuw.engr301.group9mcs.externaldata;
  * @author Bailey Jewell (jewellbail)
  */
 public class OsmOverpassGetter {
+
+    private static final URL OVERPASS_URL;
+
+    static {
+        URL overpassUrl;
+        try {
+            overpassUrl = new URL("https://lz4.overpass-api.de/api/interpreter");
+        } catch (MalformedURLException e) {
+            overpassUrl = null;
+            e.printStackTrace();
+        }
+        OVERPASS_URL = overpassUrl;
+    }
+
 
     /**
      * Determines whether or not a given location is on a forbidden area.
@@ -32,7 +50,53 @@ public class OsmOverpassGetter {
      * @return Returns the JSON response from Overpass.
      */
     private static String getAreasAtPoint(double radius, double latitude, double longitude) {
-        // TODO: Implement me!
-        return null;
+        // Firstly, we must generate the query.
+        String queryBase = "data=" +
+                "[out:json][timeout:25];\n" +
+                "(\n" +
+                "  way(around:%f, %f, %f)[building];\n" +
+                ");\n" +
+                "(._;>;);\n" +
+                "out;";
+
+        String query = String.format(queryBase, radius, latitude, longitude);
+
+        try {
+            // Setup connection.
+            HttpURLConnection connection = (HttpURLConnection) OVERPASS_URL.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("User-Agent", "Mission Control 0.1 contact jewellbail@ecs.vuw.ac.nz");
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+
+            // Setup output stream and write query.
+            OutputStream out = connection.getOutputStream(); // We keep a reference to output stream so we can close it!
+            OutputStreamWriter outWriter = new OutputStreamWriter(out, StandardCharsets.UTF_8);
+            outWriter.write(query);
+            outWriter.flush();
+            outWriter.close();
+            out.close();
+
+            // Setup the input stream and a buffer to store the response.
+            BufferedInputStream in = new BufferedInputStream(connection.getInputStream());
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+            // Reading response line by line.
+            int hasLine = in.read();
+            while(hasLine != -1) {
+                buffer.write((byte) hasLine);
+                hasLine = in.read();
+            }
+
+            String response = buffer.toString();
+            buffer.close();
+            in.close();
+            return response;
+
+        } catch (IOException e) {
+            // Connection issue.
+            e.printStackTrace();
+            return null;
+        }
     }
 }
