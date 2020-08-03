6ef25f66850c972f47fb892a5c2599c2a8856abc
Sai Panda <pandasai@ecs.vuw.ac.nz>
2020-08-03 17:05:28 +1200

    added javadoc

--- src/test/java/test/nz/ac/vuw/engr301/group9mcs/externaldata/TestNOAAGetter.java
+++ src/test/java/test/nz/ac/vuw/engr301/group9mcs/externaldata/TestNOAAGetter.java
@@ -17,6 +17,9 @@ public class TestNOAAGetter {
         return NOAAGetter.isAvailable();
     }
 
+    /**
+     * Tests if the appID is correctly set within the class
+     */
     @Test
     public void testAppID(){
         NOAAGetter testGetter = new NOAAGetter("");
@@ -24,6 +27,9 @@ public class TestNOAAGetter {
         assert testGetter.getAppId().equals("ead647e24776f26ed6f63af5f1bbf68");
     }
 
+    /**
+     * Tests that getWeatherData does not accept invalid latitude values
+     */
     @Test
     public void testInvalidLatitude() {
         assert (canConnect());
@@ -34,6 +40,9 @@ public class TestNOAAGetter {
         }
     }
 
+    /**
+     * Tests that getWeatherData does not accept invalid longitude values
+     */
     @Test
     public void testInvalidLongitude(){
         assert(canConnect());
@@ -44,12 +53,18 @@ public class TestNOAAGetter {
         }
     }
 
+    /**
+     * Tests that getWeatherData returns weather data with valid latitude and longitude
+     */
     @Test
     public void testCorrectResponse(){
         assert(canConnect());
         assert getter.getWeatherData(41, 175) != null;
     }
 
+    /**
+     * Tests that getForecast returns weather data with valid latitude and longitude
+     */
     @Test
     public void testCorrectForecastResponse(){
         assert(canConnect());
