fb58f5d39feabbe3681e6e31167a502f5846d316
Bryony Gatehouse <gatehobryo@ecs.vuw.ac.nz>
2020-08-04 17:20:24 +1200

    Add ViewObservable to GoNoGoSelectView
    After discussion with Sai on how to implement Observer pattern with
    Views - note: views already extend something.

--- src/main/java/nz/ac/vuw/engr301/group9mcs/view/GoNoGoSelectView.java
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/view/GoNoGoSelectView.java
@@ -1,6 +1,17 @@
 package nz.ac.vuw.engr301.group9mcs.view;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Observer;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
+
+import org.eclipse.jdt.annotation.Nullable;
 
 /**
  * Class for the Select Launch Perspective.
@@ -11,6 +22,28 @@ import javax.swing.JPanel;
  */
 public class GoNoGoSelectView extends JPanel{
 
+	/**
+	 * UID.
+	 */
+	private static final long serialVersionUID = 2716513907535564578L;
+	/**
+	 * The Observable that speaks to Parent.
+	 */
+	ViewObservable obs;
 	
+	/**
+	 * Sets the View up, and saves the Observer.
+	 * 
+	 * @param results 
+	 * @param o
+	 */
+	public GoNoGoSelectView(Object results, Observer o) {
+		this.obs = new ViewObservable(o);
+		
+		this.setPreferredSize(new Dimension(300, 300));
+    this.setLayout(new BorderLayout());
+    
+    this.repaint();
+	}
 	
 }
