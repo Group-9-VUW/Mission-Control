ed8a572429570d29c762be70a1d7d685e392e1d1
Bryony Gatehouse <gatehobryo@ecs.vuw.ac.nz>
2020-08-04 16:50:55 +1200

    Create GoNoGoSelectView
    An empty class that extends JPanel.

---
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/view/GoNoGoSelectView.java
@@ -0,0 +1,16 @@
+package nz.ac.vuw.engr301.group9mcs.view;
+
+import javax.swing.JPanel;
+
+/**
+ * Class for the Select Launch Perspective.
+ * Shows the Data returned from the Simulation about the speculated launch.
+ * 
+ * @author Bryony
+ *
+ */
+public class GoNoGoSelectView extends JPanel{
+
+	
+	
+}
