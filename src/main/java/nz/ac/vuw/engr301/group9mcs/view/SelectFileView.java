5113e8b612ab2864616b101576bfd68e592e8f41
Sai Panda <pandasai@ecs.vuw.ac.nz>
2020-08-04 15:02:44 +1200

    changed to GridBagLayout

--- src/main/java/nz/ac/vuw/engr301/group9mcs/view/SelectFileView.java
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/view/SelectFileView.java
@@ -5,12 +5,17 @@ package nz.ac.vuw.engr301.group9mcs.view;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.border.BevelBorder;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
@@ -23,19 +28,37 @@ public class SelectFileView extends JPanel {
 	private JFileChooser chooser; 
 	private FileNameExtensionFilter filter;
 	private final JButton chooseFileButton;
+	private JLabel fileNameLabel;
 	
 	/**
 	 * Constructor which adds the file chooser to the Panel
 	 */
 	public SelectFileView(){
-		this.setLayout(new BorderLayout());
+		this.setLayout(new GridBagLayout());
+
+		GridBagConstraints c = new GridBagConstraints();
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 1;
+		c.gridy = 0;
+		
+		this.chooseFileButton = new JButton("Choose File");
+		
+		this.add(this.chooseFileButton, c);
+		
 		this.chooser = new JFileChooser();
 		this.filter = new FileNameExtensionFilter("Open Rocket Files", "ork");
-		this.chooseFileButton = new JButton("Choose File");
-		//this.chooseFileButton.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
-		this.chooseFileButton.setPreferredSize(new Dimension(60, 20));
-		this.add(BorderLayout.NORTH, this.chooseFileButton);
-		this.setPreferredSize(new Dimension(300, 300));
+		
+		
+		c.gridx = 1;
+		c.gridy = 1;
+		c.ipady = 20;
+		this.fileNameLabel = new JLabel("No File Selected", SwingConstants.CENTER);
+
+		this.add(this.fileNameLabel, c);
+//		this.add(BorderLayout.NORTH, this.chooseFileButton);
+//		this.add(BorderLayout.CENTER, this.fileNameLabel);
+		this.setPreferredSize(new Dimension(300, 100));
 		
 	}
 	
