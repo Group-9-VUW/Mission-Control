8196879b3182ca28e5833a01dc8af58f3fe7ff92
Bryony Gatehouse <gatehobryo@ecs.vuw.ac.nz>
2020-08-06 21:03:54 +1200

    Add first Panel to Perspective
    Saves the filename, passes a ViewObservable with the Observer inside,
    receives the file name through update

--- src/main/java/nz/ac/vuw/engr301/group9mcs/controller/SelectSitePerspective.java
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/controller/SelectSitePerspective.java
@@ -5,29 +5,54 @@ import java.util.Observer;
 
 import javax.swing.JPanel;
 
+import org.eclipse.jdt.annotation.Nullable;
+
+import nz.ac.vuw.engr301.group9mcs.view.SelectFileView;
+import nz.ac.vuw.engr301.group9mcs.view.ViewObservable;
+
 /**
  * Perspective that holds the Panels for the Selecting a Launch Site.
  * 
  * @author Bryony
  *
  */
-public class SelectSitePerspective extends Observable implements Perspective {
+public class SelectSitePerspective extends Observable implements Perspective, Observer {
 
 	/**
 	 * The Panel displayed on the screen that holds all other panels.
 	 */
 	private JPanel panel;
+	private String filename;
 	
 	/**
 	 * Create the Perspective and construct the Panel.
 	 */
 	public SelectSitePerspective() {
 		this.panel = new JPanel();
+		this.filename = "";
 	}
 	
 	@Override
 	public JPanel enable(MenuController menu) {
+		start();
 		return this.panel;
+	}
+	
+	/**
+	 * Start the Perspective at the File Selection.
+	 */
+	private void start() {
+		this.panel.add(new SelectFileView(new ViewObservable(this)));
+	}
+	
+	private void second() {
+		this.panel.removeAll();
+		// this.panel.add(SelectSiteView(....
+	}
+	
+	private void third() {
+		this.panel.removeAll();
+		// this.panel.add(GoNoGoView(....
 	}
 
 	@Override
@@ -38,6 +63,16 @@ public class SelectSitePerspective extends Observable implements Perspective {
 	@Override
 	public String name() {
 		return "select";
+	}
+
+	@Override
+	public void update(@Nullable Observable o, @Nullable Object arg) {
+		if(arg != null) {
+			if(arg instanceof String) {
+				this.filename = (String) arg;
+				second();
+			}
+		}
 	}
 	
 }
