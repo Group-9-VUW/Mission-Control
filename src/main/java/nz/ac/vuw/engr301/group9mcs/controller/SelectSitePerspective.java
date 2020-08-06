e6f4b44ea50f96b786c7cb799bfcb9f02f46f37f
Claire Chambers <chambeconn@ecs.vuw.ac.nz>
2020-08-06 21:01:41 +1200

    Made SelectSitePerspective implement Observer, added sub panels

--- src/main/java/nz/ac/vuw/engr301/group9mcs/controller/SelectSitePerspective.java
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/controller/SelectSitePerspective.java
@@ -1,9 +1,18 @@
 package nz.ac.vuw.engr301.group9mcs.controller;
 
+import java.awt.BorderLayout;
 import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.JPanel;
+
+import org.eclipse.jdt.annotation.Nullable;
+
+import nz.ac.vuw.engr301.group9mcs.commons.Condition;
+import nz.ac.vuw.engr301.group9mcs.commons.Null;
+import nz.ac.vuw.engr301.group9mcs.commons.PreconditionViolationException;
+import nz.ac.vuw.engr301.group9mcs.view.SelectFileView;
+import nz.ac.vuw.engr301.group9mcs.view.ViewObservable;
 
 /**
  * Perspective that holds the Panels for the Selecting a Launch Site.
@@ -11,18 +20,22 @@ import javax.swing.JPanel;
  * @author Bryony
  *
  */
-public class SelectSitePerspective extends Observable implements Perspective {
+public class SelectSitePerspective extends Observable implements Perspective, Observer {
 
 	/**
 	 * The Panel displayed on the screen that holds all other panels.
 	 */
 	private JPanel panel;
 	
+	private final JPanel fileGet = new SelectFileView(new ViewObservable(this));
+	private final JPanel siteMap = new JPanel();
+	private final JPanel resultsShow = new JPanel();
+	
 	/**
 	 * Create the Perspective and construct the Panel.
 	 */
 	public SelectSitePerspective() {
-		this.panel = new JPanel();
+		this.panel = new JPanel(new BorderLayout());
 	}
 	
 	@Override
@@ -38,6 +51,27 @@ public class SelectSitePerspective extends Observable implements Perspective {
 	@Override
 	public String name() {
 		return "select";
+	}
+
+	@Override
+	public void update(@Nullable Observable o, @Nullable Object arg) {
+		if(arg instanceof String[])
+		{
+			String[] args = (String[]) Null.nonNull(arg);
+			Condition.PRE.positive("args.length", args.length);
+			
+			switch(args[0])
+			{
+				default:
+					throw new PreconditionViolationException("Unregonized command sent to SelectSitePerspective");
+			}
+		}
+	}
+	
+	private void switchTo(JPanel newPanel)
+	{
+		this.panel.removeAll();
+		this.panel.add(newPanel, BorderLayout.CENTER);
 	}
 	
 }
