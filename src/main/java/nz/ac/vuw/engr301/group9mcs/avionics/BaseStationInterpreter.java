ce82bb839114cb4f519074e117aed67800377200
Sai Panda <pandasai@ecs.vuw.ac.nz>
2020-08-11 09:39:40 +1200

    setup classes for basestationinterpreter and rocket data

---
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/avionics/BaseStationInterpreter.java
@@ -0,0 +1,10 @@
+package nz.ac.vuw.engr301.group9mcs.avionics;
+
+/**
+ * This will parse incoming data from the rocket and will create objects contained the
+ * parsed information. The incoming data is in CSV format.
+ */
+public class BaseStationInterpreter {
+
+
+}
---
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/commons/RocketData.java
@@ -0,0 +1,140 @@
+package nz.ac.vuw.engr301.group9mcs.commons;
+
+/**
+ * Holds one instance of rocket data.
+ */
+public class RocketData {
+
+    /**
+     * Possible values for the rocket state value returned from the base station.
+     */
+    public static enum ROCKET_STATE {
+        ARMED,
+        FLIGHT
+    }
+
+    /**
+     * This is the current clock time on the rocket.
+     */
+    private final double timestamp;
+
+    /**
+     * Latitude of the rocket.
+     */
+    private final double latitude;
+
+    /**
+     * Longitude of the rocket.
+     */
+    private final double longitude;
+
+    /**
+     * The x position of the gimbal.
+     */
+    private final double gimbalX;
+
+    /**
+     * The y position of the gimbal;
+     */
+    private final double gimbalY;
+
+    /**
+     * The IMU (Inertial measurement unit) reading for the x axis acceleration
+     * of the rocket.
+     */
+    private final double accelerationX;
+
+    /**
+     * The IMU (Inertial measurement unit) reading for the y axis acceleration
+     * of the rocket.
+     */
+    private final double accelerationY;
+
+    /**
+     * The IMU (Inertial measurement unit) reading for the z axis acceleration
+     * of the rocket.
+     */
+    private final double accelerationZ;
+
+    /**
+     * The IMU (Inertial measurement unit) reading for the z axis rotation
+     * of the rocket.
+     */
+    private final double rotationX;
+
+    /**
+     * The IMU (Inertial measurement unit) reading for the z axis rotation
+     * of the rocket.
+     */
+    private final double rotationY;
+
+    /**
+     * The IMU (Inertial measurement unit) reading for the z axis rotation
+     * of the rocket.
+     */
+    private final double rotationZ;
+
+    /**
+     * The barometer reading for the humidity outside the rocket.
+     */
+    private final double humidity;
+
+    /**
+     * The barometer reading for the humidity outside the rocket.
+     */
+    private final double temperature;
+
+    /**
+     * The barometer reading for the humidity outside the rocket.
+     */
+    private final double airPressure;
+
+    /**
+     * Battery status (need to confirm exact meaning of this).
+     */
+    private final double batteryStatus;
+
+    /**
+     * The state of the rocket.
+     */
+    private final ROCKET_STATE state;
+
+    /**
+     * Initialises all fields.
+     * @param timestamp current clock on the rocket.
+     * @param latitude latitude of the rocket.
+     * @param longitude longitude of the rocket.
+     * @param gimbalX x position of the gimbal.
+     * @param gimbalY y position of the gimbal.
+     * @param accelerationX acceleration in the x axis direction.
+     * @param accelerationY acceleration in the y axis direction.
+     * @param accelerationZ acceleration in the z axis direction.
+     * @param rotationX rotation about the x axis.
+     * @param rotationY rotation about the y axis.
+     * @param rotationZ rotation about the z axis.
+     * @param humidity humidty outside the rocket.
+     * @param temperature temperature outside the rocket.
+     * @param airPressure air pressure outside the rocket.
+     * @param batteryStatus battery status of the rocket.
+     * @param state state of the rocket.
+     */
+    public RocketData(double timestamp, double latitude, double longitude, double gimbalX, double gimbalY, double accelerationX, double accelerationY, double accelerationZ, double rotationX, double rotationY, double rotationZ, double humidity, double temperature, double airPressure, double batteryStatus, ROCKET_STATE state){
+        this.timestamp = timestamp;
+        this.latitude = latitude;
+
+        this.longitude = longitude;
+        this.gimbalX = gimbalX;
+        this.gimbalY = gimbalY;
+        this.accelerationX = accelerationX;
+        this.accelerationY = accelerationY;
+        this.accelerationZ = accelerationZ;
+        this.rotationX = rotationX;
+        this.rotationY = rotationY;
+        this.rotationZ = rotationZ;
+        this.humidity = humidity;
+        this.temperature = temperature;
+        this.airPressure = airPressure;
+        this.batteryStatus = batteryStatus;
+        this.state = state;
+    }
+}
