5d8cc0ef1e4dd0dad75bb943cc7cbbbff83af33c
Sai Panda <pandasai@ecs.vuw.ac.nz>
2020-09-04 09:38:55 +1200

    added checkNormalPython()

--- src/main/java/nz/ac/vuw/engr301/group9mcs/commons/PythonContext.java
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/commons/PythonContext.java
@@ -15,7 +15,7 @@ import java.util.Scanner;
 public class PythonContext {
 	/**
 	 * This is the python command to run python on the users machine. 
-	 * If the python3 command runs, then the user should be able to successfully
+	 * If the 'python3' command runs (or 'python'), then the user should be able to successfully
      * run the NOAA python script (provided that they have the required libraries).
 	 */
     public static String pythonCommand = "python3";
@@ -26,7 +26,9 @@ public class PythonContext {
      * @throws IOException if the command cannot be ran. 
      */
 
-	public static boolean hasValidPython() throws IOException{
+	public static boolean hasValidPython() {
+	    // This will first try run the 'python3' command, if that does not exist
+        // on the users machine, it will then try to run the normal 'python' command.
         String currentCommand = "python3 --version";
         try {
             Process process = Runtime.getRuntime().exec(currentCommand);
@@ -37,55 +39,69 @@ public class PythonContext {
             String pythonThreeOutput = reader.readLine();
 
             if (pythonThreeOutput != null){
-                //If python3 gives output, then the user has python 3 installed.
+                // If python3 gives output, then the user has python 3 installed.
                 return true;
             } else {
-                //The normal "python" command can also be used to check the python version,
-                //so we check with that command aswell and verify if the version is python three.
-                process = Runtime.getRuntime().exec("python --version");
-                is = process.getInputStream();
-                reader = new BufferedReader(new InputStreamReader(is));
-                String pythonVersion = reader.readLine();
-
-                if(pythonVersion == null || !pythonVersion.contains("Python")){
-                    DefaultLogger.logger.error("User does not have python installed.");
-                    return false;
-                }
-
-                String[] split = pythonVersion.split(" ");
-                // Sample output here is
-                // Python x.y.z
-                // Checking the value of 'x' is sufficient enough to check if the user
-                // has a python version >= 3 installed.
-                if(Character.getNumericValue(split[1].charAt(0)) < 3){
-                    DefaultLogger.logger.error("User has python version " + split[1] + ". Minimum python version" +
-                            "needed to run rocketpyalpha is Python 3.");
-                    return false;
-                } else {
-                    pythonCommand = "python";
-                    return true;
-                }
+                // Else try to run the normal 'python' command.
+                currentCommand = "python --version";
+                return checkNormalPython();
             }
-        } catch (IOException pythonThreeException) {
+        } catch (IOException e) {
             DefaultLogger.logger.error("Error running " + currentCommand);
 
-            //throw new IOException("Error running " + currentCommand);
-            currentCommand = "python --version";
-            try{
-                return checkNormalPython();
-            } catch (IOException normalPythonException){
-                DefaultLogger.logger.error("Error running " + currentCommand);
-                throw new IOException("Could not run both 'python3' and 'python'");
+            // If we have already checked both 'python3' and 'python', then return false.
+            if(currentCommand.equals("python --version")){
+                return false;
+            } else {
+                // Otherwise, try to run the normal 'python' command.
+                try{
+                    return checkNormalPython();
+                } catch (IOException normalPythonExeception) {
+                    DefaultLogger.logger.error("Could not run both 'python3 --version' and 'python --version'");
+                    return false;
+                }
             }
+
         }
     }
 
+    /**
+     * Checks the users python version using the normal 'python' command as opposed to 'python3'.
+     * @return true if the users python version is >= 3, false otherwise
+     * @throws IOException if the 'python --version' command cannot be ran on the users machine.
+     */
     private static boolean checkNormalPython() throws IOException{
+        // The normal "python" command can also be used to check the python version,
+        // so we check with that command aswell and verify if the version is python three.
+        Process process = Runtime.getRuntime().exec("python --version");
+        InputStream is = process.getInputStream();
 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+
+        String pythonVersion = reader.readLine();
+
+        if(pythonVersion == null || !pythonVersion.contains("Python")){
+            DefaultLogger.logger.error("User does not have python installed.");
+            return false;
+        }
+
+        String[] split = pythonVersion.split(" ");
+        // Sample output here is
+        // Python x.y.z
+        // Checking the value of 'x' is sufficient enough to check if the user
+        // has a python version >= 3 installed.
+        if(Character.getNumericValue(split[1].charAt(0)) < 3){
+            DefaultLogger.logger.error("User has python version " + split[1] + ". Minimum python version" +
+                    "needed to run rocketpyalpha is Python 3.");
+            return false;
+        } else {
+            pythonCommand = "python";
+            return true;
+        }
     }
 
 
     public static void main(String[] args) throws IOException{
-        System.out.println(checkPythonThree());
+        System.out.println(hasValidPython());
     }
 }
