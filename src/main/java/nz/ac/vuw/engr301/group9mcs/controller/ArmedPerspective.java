1cb7069a1a28902ac61141b861c8a6df2d6980a8
gatehobryo <gatehobryo@ecs.vuw.ac.nz>
2020-09-08 10:23:12 +1200

    Add fake panels to Armed Perspective
    Show where the two panels would be. Will be in same view, don't need to
    change panels.

--- src/main/java/nz/ac/vuw/engr301/group9mcs/controller/ArmedPerspective.java
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/controller/ArmedPerspective.java
@@ -1,12 +1,17 @@
 package nz.ac.vuw.engr301.group9mcs.controller;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.JPanel;
 
 import org.eclipse.jdt.annotation.Nullable;
+
+import nz.ac.vuw.engr301.group9mcs.commons.Null;
 
 /**
  * Perspective that holds the Panels for the View while the rocket is Armed.
@@ -19,16 +24,55 @@ public class ArmedPerspective extends Observable implements Perspective, Observe
 	/**
 	 * The Panel displayed on the screen that holds all other panels.
 	 */
-	private JPanel panel;
+	JPanel panel;
 	
-	// WARNING PANEL : armed and dangerous, do not touch rocket -> close when rocket is fired
-	// ROCKET DATA PANEL : text output from rocket?
+	/** 
+	 * WARNING PANEL : armed and dangerous, do not touch rocket -> close when rocket is fired
+	 */
+	private JPanel warningPanel;
+	
+	/** 
+	 * ROCKET DATA PANEL : text output from rocket?
+	 */
+	private JPanel rocketDataPanel;
 	
 	/**
 	 * Construct the Panel
 	 */
 	public ArmedPerspective() {
 		this.panel = new JPanel(new BorderLayout());
+		// TODO: A proper Rocket Data Panel
+		this.rocketDataPanel = new JPanel() {
+
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1254759273362687813L;
+			
+			@Override
+			protected void paintComponent(@Nullable Graphics g) {
+				Null.nonNull(g).setColor(Color.green);
+				Null.nonNull(g).fillRect(0, 0, this.getWidth(), this.getHeight());
+			}
+		};
+		this.rocketDataPanel.setPreferredSize(new Dimension(400, 300));
+		// TODO: A proper Warning Panel : "Armed and Dangerous", "Do not touch while armed"
+		this.warningPanel = new JPanel() {
+
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = -2650026648197700627L;
+			
+			@Override
+			protected void paintComponent(@Nullable Graphics g) {
+				Null.nonNull(g).setColor(Color.red);
+				Null.nonNull(g).fillRect(0, 0, this.getWidth(), this.getHeight());
+			}
+		};
+		this.warningPanel.setPreferredSize(new Dimension(400, 100));
+		this.panel.add(this.rocketDataPanel, BorderLayout.CENTER);
+		this.panel.add(this.warningPanel, BorderLayout.NORTH);
 	}
 
 	@Override
