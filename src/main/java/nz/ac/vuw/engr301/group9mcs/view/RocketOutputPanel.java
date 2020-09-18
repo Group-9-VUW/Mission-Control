5b1111b2d78213f9359f19860afd1b8da358c261
gatehobryo <gatehobryo@ecs.vuw.ac.nz>
2020-09-18 12:32:51 +1200

    Add Update method to RocketOutputPanel to replace Data

--- src/main/java/nz/ac/vuw/engr301/group9mcs/view/RocketOutputPanel.java
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/view/RocketOutputPanel.java
@@ -5,6 +5,8 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
+import nz.ac.vuw.engr301.group9mcs.commons.RocketData;
+
 /**
  * Displays the information returned from the rocket.
  * 
@@ -17,14 +19,14 @@ public class RocketOutputPanel extends JPanel{
 	 * 
 	 */
 	private static final long serialVersionUID = -4163333043424348578L;
-
+	
 	/**
 	 * Text area to display rocket output.
 	 */
 	private JTextArea textArea;
 
 	/**
-	 * Creates the Panel -> should be passed a listener/other that it connected to the rocket output.
+	 * Creates the Panel
 	 */
 	public RocketOutputPanel() {
 		this.setLayout(new BorderLayout());
@@ -34,6 +36,16 @@ public class RocketOutputPanel extends JPanel{
 		this.textArea.setEditable(false);
 		this.textArea.setCaretPosition(this.textArea.getDocument().getLength());
 		this.add(scrollPane, BorderLayout.CENTER);
+	}
+	
+	/**
+	 * Data will be converted to String format and pasted onto screen.
+	 * It will then be discarded.
+	 * 
+	 * @param data Rocket Data to be displayed on screen.
+	 */
+	public void passRocketData(RocketData data) {
+		this.textArea.setText(data.toString() + "\n");
 	}
 
 }
