5f04bdcf8bf7582eb7c7d3e8eebe68c093287c5f
hindlejosh <hindlejosh@petit-lyon.ecs.vuw.ac.nz>
2020-09-22 10:52:26 +1200

    Fixed format and removed TODO

--- src/main/java/nz/ac/vuw/engr301/group9mcs/commons/LongLatHelper.java
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/commons/LongLatHelper.java
@@ -1,11 +1,11 @@
 /**
- * 
+ *
  */
 package nz.ac.vuw.engr301.group9mcs.commons;
 
 /**
  * A helper class for dealing with long/lat coordinates
- * 
+ *
  * @author Claire
  */
 public final class LongLatHelper {
@@ -13,50 +13,45 @@ public final class LongLatHelper {
 	/**
 	 * Returns how many kilometers exist between degrees of latitude (Distance between the poles)
 	 * at the specific latitude given.
-	 * 
+	 *
 	 * @param latitude The latitude you're measuring from
 	 * @return The number of kilometers for every degree of latitude
 	 */
-	public static double kilometersPerDegreeOfLatitude(double latitude)
-	{
-		//TODO: implement ellipsoidal formulas
+	public static double kilometersPerDegreeOfLatitude(double latitude)	{
 		return 110.567;
 	}
-	
+
 	/**
 	 * Returns how many kilometers exist between degrees of longditude (Distance between east and west)
 	 * at the specific latitude given.
-	 * 
+	 *
 	 * @param latitude The latitude you're measuring from
 	 * @return The number of kilometers for every degree of longditude
 	 */
-	public static double kilometeresPerDegreeOfLongitude(double latitude)
-	{
+	public static double kilometeresPerDegreeOfLongitude(double latitude) {
 		return 111.3215 * Math.cos(Math.toRadians(Math.abs(latitude)));
 	}
-	
+
 	/**
 	 * Returns the latitude N kilometers north of a given latitude
 	 * @param latitude The current latitude
 	 * @param N The number of kilometers to go north
 	 * @return The latitude N kilometers north
 	 */
-	public static double latitudeNKilometersNorth(double latitude, double N)
-	{
+	public static double latitudeNKilometersNorth(double latitude, double N) {
 		return latitude + (N / kilometersPerDegreeOfLatitude(latitude));
 	}
-	
+
 	/**
 	 * Returns the latitude N kilometers south of a given latitude
 	 * @param latitude The current latitude
 	 * @param N The number of kilometers to go south
 	 * @return The latitude N kilometers south
 	 */
-	public static double latitudeNKilometersSouth(double latitude, double N)
-	{
+	public static double latitudeNKilometersSouth(double latitude, double N) {
 		return latitude - (N / kilometersPerDegreeOfLatitude(latitude));
 	}
-	
+
 	/**
 	 * Returns the longitude N kilometers east of a given longitude
 	 * @param latitude The current latitude
@@ -64,11 +59,10 @@ public final class LongLatHelper {
 	 * @param N The number of kilometers to go east
 	 * @return The longitude N kilometers east
 	 */
-	public static double longditudeNKilometersEast(double latitude, double longitude, double N)
-	{
+	public static double longditudeNKilometersEast(double latitude, double longitude, double N)	{
 		return longitude + (N / kilometeresPerDegreeOfLongitude(latitude));
 	}
-	
+
 	/**
 	 * Returns the longitude N kilometers west of a given longitude
 	 * @param latitude The current latitude
@@ -76,9 +70,7 @@ public final class LongLatHelper {
 	 * @param N The number of kilometers to go west
 	 * @return The longitude N kilometers west
 	 */
-	public static double longditudeNKilometersWest(double latitude, double longitude, double N)
-	{
+	public static double longditudeNKilometersWest(double latitude, double longitude, double N)	{
 		return longitude - (N / kilometeresPerDegreeOfLongitude(latitude));
 	}
-	
 }
---
+++ src/test/java/test/nz/ac/vuw/engr301/group9mcs/commons/TestLongLatHelper.java
@@ -0,0 +1,10 @@
+package test.nz.ac.vuw.engr301.group9mcs.commons;
+
+/**
+ * Tests the LongLatHelper class and the methods within.
+ *
+ * @author Joshua
+ */
+public class TestLongLatHelper {
+
+}
