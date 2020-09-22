c0305a5beeceb0fa3b77d3b3767f42babd53ce9f
gatehobryo <gatehobryo@ecs.vuw.ac.nz>
2020-09-22 12:15:41 +1200

    Add JavaDoc Comments and Null checks

--- src/main/java/nz/ac/vuw/engr301/group9mcs/externaldata/OsmOverpassData.java
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/externaldata/OsmOverpassData.java
@@ -2,6 +2,8 @@ package nz.ac.vuw.engr301.group9mcs.externaldata;
 
 import org.eclipse.jdt.annotation.Nullable;
 
+import nz.ac.vuw.engr301.group9mcs.commons.Null;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -13,107 +15,134 @@ import java.util.Map;
  */
 public class OsmOverpassData {
 
-    private final List<Node> NODES;
-    private final List<Way> WAYS;
+	/**
+	 * List of nodes from Overpass.
+	 */
+	private final List<Node> NODES;
+	/**
+	 * List of ways from Overpass.
+	 */
+	private final List<Way> WAYS;
 
-    /**
-     * Creates an Overpass dataset from lists of nodes and ways.
-     *
-     * @param nodes List of nodes from Overpass.
-     * @param ways List of ways from Overpass.
-     */
-    OsmOverpassData(List<Node> nodes, List<Way> ways) {
-        this.NODES = nodes;
-        this.WAYS = ways;
-    }
+	/**
+	 * Creates an Overpass dataset from lists of nodes and ways.
+	 *
+	 * @param nodes List of nodes from Overpass.
+	 * @param ways List of ways from Overpass.
+	 */
+	OsmOverpassData(List<Node> nodes, List<Way> ways) {
+		this.NODES = nodes;
+		this.WAYS = ways;
+	}
 
-    /**
-     * Gets an unmodifiable list of nodes from the dataset.
-     * @return Returns an unmodifiable list of nodes from the dataset.
-     */
-    public List<Node> getNodes() {
-        return Collections.unmodifiableList(this.NODES);
-    }
+	/**
+	 * Gets an unmodifiable list of nodes from the dataset.
+	 * @return Returns an unmodifiable list of nodes from the dataset.
+	 */
+	public List<Node> getNodes() {
+		return Null.nonNull(Collections.unmodifiableList(this.NODES));
+	}
 
-    /**
-     * Gets an unmodifiable list of ways from the dataset.
-     * @return Returns an unmodifiable list of ways from the dataset.
-     */
-    public List<Way> getWays() {
-        return Collections.unmodifiableList(this.WAYS);
-    }
+	/**
+	 * Gets an unmodifiable list of ways from the dataset.
+	 * @return Returns an unmodifiable list of ways from the dataset.
+	 */
+	public List<Way> getWays() {
+		return Null.nonNull(Collections.unmodifiableList(this.WAYS));
+	}
 
-    /**
-     * Represents a single OpenStreetMap node. Every OSM entity comprises nodes.
-     */
-    public static final class Node {
-        final long ID;
-        final double LAT;
-        final double LON;
-        private final Map<String, String> TAGS;
+	/**
+	 * Represents a single OpenStreetMap node. Every OSM entity comprises nodes.
+	 */
+	public static final class Node {
+		/**
+		 * Node ID. This is the node's global OSM ID.
+		 */
+		final long ID;
+		/**
+		 * Latitude of the node.
+		 */
+		final double LAT;
+		/**
+		 * Longitude of the node.
+		 */
+		final double LON;
+		/**
+		 * Tags associated with the node, describing the node's purpose.
+		 */
+		private final Map<String, String> TAGS;
 
-        /**
-         * Creates a single node object from the given OSM data.
-         *
-         * @param id Node ID. This is the node's global OSM ID.
-         * @param lat Latitude of the node.
-         * @param lon Longitude of the node.
-         * @param tags Tags associated with the node, describing the node's purpose.
-         */
-        Node(long id, double lat, double lon, @Nullable Map<String, String> tags) {
-            this.ID = id;
-            this.LAT = lat;
-            this.LON = lon;
-            this.TAGS = tags != null ? tags : Collections.emptyMap();
-        }
+		/**
+		 * Creates a single node object from the given OSM data.
+		 *
+		 * @param id Node ID. This is the node's global OSM ID.
+		 * @param lat Latitude of the node.
+		 * @param lon Longitude of the node.
+		 * @param tags Tags associated with the node, describing the node's purpose.
+		 */
+		Node(long id, double lat, double lon, @Nullable Map<String, String> tags) {
+			this.ID = id;
+			this.LAT = lat;
+			this.LON = lon;
+			this.TAGS = tags != null ? tags : Null.nonNull(Collections.emptyMap());
+		}
 
-        /**
-         * Gets an unmodifiable list of tags associated with the node. Tags describe the purpose of
-         * a node. Nodes that are members of ways will generally not have their own tags, preferring
-         * to defer to their parent way.
-         *
-         * @return Returns unmodifiable map of tags associated with the way.
-         */
-        public Map<String, String> getTags() {
-            return Collections.unmodifiableMap(this.TAGS);
-        }
-    }
+		/**
+		 * Gets an unmodifiable list of tags associated with the node. Tags describe the purpose of
+		 * a node. Nodes that are members of ways will generally not have their own tags, preferring
+		 * to defer to their parent way.
+		 *
+		 * @return Returns unmodifiable map of tags associated with the way.
+		 */
+		public Map<String, String> getTags() {
+			return Null.nonNull(Collections.unmodifiableMap(this.TAGS));
+		}
+	}
 
-    /**
-     * Represents a single OpenStreetMap way. Ways comprise nodes.
-     */
-    public static final class Way {
-        final long ID;
-        private final List<Node> NODES;
-        private final Map<String, String> TAGS;
+	/**
+	 * Represents a single OpenStreetMap way. Ways comprise nodes.
+	 */
+	public static final class Way {
+		/**
+		 * Way ID. This is the way's global OSM ID.
+		 */
+		final long ID;
+		/**
+		 * List of nodes composing the way.
+		 */
+		private final List<Node> NODES;
+		/**
+		 * Tags associated with the node, describing the node's purpose.
+		 */
+		private final Map<String, String> TAGS;
 
-        /**
-         * Creates a single way object from the given OSM data.
-         *
-         * @param id Way ID. This is the way's global OSM ID.
+		/**
+		 * Creates a single way object from the given OSM data.
+		 *
+		 * @param id Way ID. This is the way's global OSM ID.
 		 * @param nodes List of nodes composing the way.
-         * @param tags Tags associated with the node, describing the node's purpose.
-         */
-        Way(long id, List<Node> nodes, @Nullable Map<String, String> tags) {
-            this.ID = id;
-            this.NODES = nodes;
-            this.TAGS = tags != null ? tags : Collections.emptyMap();
-        }
+		 * @param tags Tags associated with the node, describing the node's purpose.
+		 */
+		Way(long id, List<Node> nodes, @Nullable Map<String, String> tags) {
+			this.ID = id;
+			this.NODES = nodes;
+			this.TAGS = tags != null ? tags : Null.nonNull(Collections.emptyMap());
+		}
 
-        /**
-         * Gets an unmodifiable list of nodes composing the way.
-         * @return Returns unmodifiable list of nodes composing the way.
-         */
-        public List<Node> getNodes() {
-            return Collections.unmodifiableList(this.NODES);
-        }
+		/**
+		 * Gets an unmodifiable list of nodes composing the way.
+		 * @return Returns unmodifiable list of nodes composing the way.
+		 */
+		public List<Node> getNodes() {
+			return Null.nonNull(Collections.unmodifiableList(this.NODES));
+		}
 
-        /**
-         * Gets an unmodifiable list of tags associated with the way.
-         * @return Returns unmodifiable map of tags associated with the way.
-         */
-        public Map<String, String> getTags() {
-            return Collections.unmodifiableMap(this.TAGS);
-        }
-    }
+		/**
+		 * Gets an unmodifiable list of tags associated with the way.
+		 * @return Returns unmodifiable map of tags associated with the way.
+		 */
+		public Map<String, String> getTags() {
+			return Null.nonNull(Collections.unmodifiableMap(this.TAGS));
+		}
+	}
 }
