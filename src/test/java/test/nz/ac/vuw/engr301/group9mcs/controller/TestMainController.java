98b13147670046f923f46ae07856beb9f49c4172
gatehobryo <gatehobryo@ecs.vuw.ac.nz>
2020-09-25 10:56:59 +1200

    Catch Headless exception

--- src/test/java/test/nz/ac/vuw/engr301/group9mcs/controller/TestMainController.java
+++ src/test/java/test/nz/ac/vuw/engr301/group9mcs/controller/TestMainController.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.awt.Component;
+import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -38,7 +39,7 @@ public final class TestMainController {
 	 * MainController to Test.
 	 */
 	private MainController m;
-	
+
 	/**
 	 * Reset basic Elements
 	 */
@@ -50,97 +51,109 @@ public final class TestMainController {
 		this.m.setVisible(false);
 		this.m.setFocusable(false);
 	}
-	
+
 	/**
 	 * Checks that Panel is not added when Perspective added.
 	 */
 	@SuppressWarnings("null")
 	@Test
 	public void testPerspectiveAdd() {
-		setup();
-		this.m.addPerspective(this.fp.name(), this.fp);
-		Component[] mCom = this.m.getComponents();
-		JRootPane jrPane = (JRootPane)mCom[0];
-		Component[] jrCom = jrPane.getComponents();
-		JLayeredPane jlPane;
-		if(jrCom[0] instanceof JLayeredPane) {
-			jlPane = (JLayeredPane)jrCom[0];
-		} else {
-			jlPane = (JLayeredPane)jrCom[1];
+		try {
+			setup();
+			this.m.addPerspective(this.fp.name(), this.fp);
+			Component[] mCom = this.m.getComponents();
+			JRootPane jrPane = (JRootPane)mCom[0];
+			Component[] jrCom = jrPane.getComponents();
+			JLayeredPane jlPane;
+			if(jrCom[0] instanceof JLayeredPane) {
+				jlPane = (JLayeredPane)jrCom[0];
+			} else {
+				jlPane = (JLayeredPane)jrCom[1];
+			}
+			Component[] jlCom = jlPane.getComponents();
+			JPanel jPanel;
+			if(jlCom[0] instanceof JPanel) {
+				jPanel = (JPanel)jlCom[0];
+			} else {
+				jPanel = (JPanel)jlCom[1];
+			}
+			Component[] jPCom = jPanel.getComponents();
+			JPanel jPanel2 = (JPanel)jPCom[0];
+			Component[] jP2Com = jPanel2.getComponents();
+			assertTrue(jP2Com.length == 0);
+		} catch (@SuppressWarnings("unused") HeadlessException e) {
+			System.out.println("Test failed to run due to lack of monitor");
 		}
-		Component[] jlCom = jlPane.getComponents();
-		JPanel jPanel;
-		if(jlCom[0] instanceof JPanel) {
-			jPanel = (JPanel)jlCom[0];
-		} else {
-			jPanel = (JPanel)jlCom[1];
-		}
-		Component[] jPCom = jPanel.getComponents();
-		JPanel jPanel2 = (JPanel)jPCom[0];
-		Component[] jP2Com = jPanel2.getComponents();
-		assertTrue(jP2Com.length == 0);
 	}
-	
+
 	/**
 	 * Checks that Panel is added when Perspective set.
 	 */
 	@SuppressWarnings("null")
 	@Test
 	public void testPerspectiveSet() {
-		setup();
-		this.m.addPerspective(this.fp.name(), this.fp);
-		this.m.setPerspective(this.fp.name());
-		Component[] mCom = this.m.getComponents();
-		JRootPane jrPane = (JRootPane)mCom[0];
-		Component[] jrCom = jrPane.getComponents();
-		JLayeredPane jlPane;
-		if(jrCom[0] instanceof JLayeredPane) {
-			jlPane = (JLayeredPane)jrCom[0];
-		} else {
-			jlPane = (JLayeredPane)jrCom[1];
+		try {
+			setup();
+			this.m.addPerspective(this.fp.name(), this.fp);
+			this.m.setPerspective(this.fp.name());
+			Component[] mCom = this.m.getComponents();
+			JRootPane jrPane = (JRootPane)mCom[0];
+			Component[] jrCom = jrPane.getComponents();
+			JLayeredPane jlPane;
+			if(jrCom[0] instanceof JLayeredPane) {
+				jlPane = (JLayeredPane)jrCom[0];
+			} else {
+				jlPane = (JLayeredPane)jrCom[1];
+			}
+			Component[] jlCom = jlPane.getComponents();
+			JPanel jPanel;
+			if(jlCom[0] instanceof JPanel) {
+				jPanel = (JPanel)jlCom[0];
+			} else {
+				jPanel = (JPanel)jlCom[1];
+			}
+			Component[] jPCom = jPanel.getComponents();
+			JPanel jPanel2 = (JPanel)jPCom[0];
+			Component[] jP2Com = jPanel2.getComponents();
+			JPanel first = (JPanel)jP2Com[0];
+			assertTrue(first.getName().equals(this.pa.getName()));
+		} catch (@SuppressWarnings("unused") HeadlessException e) {
+			System.out.println("Test failed to run due to lack of monitor");
 		}
-		Component[] jlCom = jlPane.getComponents();
-		JPanel jPanel;
-		if(jlCom[0] instanceof JPanel) {
-			jPanel = (JPanel)jlCom[0];
-		} else {
-			jPanel = (JPanel)jlCom[1];
-		}
-		Component[] jPCom = jPanel.getComponents();
-		JPanel jPanel2 = (JPanel)jPCom[0];
-		Component[] jP2Com = jPanel2.getComponents();
-		JPanel first = (JPanel)jP2Com[0];
-		assertTrue(first.getName().equals(this.pa.getName()));
 	}
-	
+
 	/**
-	 * 
+	 * Checks that Frame is disabled when exit button clicked.
 	 */
 	@Test
 	public void testClose() {
-		setup();
-		assertTrue(this.m.isDisplayable());
-		Component[] mCom = this.m.getComponents();
-		JRootPane jrPane = (JRootPane)mCom[0];
-		Component[] jrCom = jrPane.getComponents();
-		JLayeredPane jlPane;
-		if(jrCom[0] instanceof JLayeredPane) {
-			jlPane = (JLayeredPane)jrCom[0];
-		} else {
-			jlPane = (JLayeredPane)jrCom[1];
+		try {
+			setup();
+			assertTrue(this.m.isDisplayable());
+			Component[] mCom = this.m.getComponents();
+			JRootPane jrPane = (JRootPane)mCom[0];
+			Component[] jrCom = jrPane.getComponents();
+			JLayeredPane jlPane;
+			if(jrCom[0] instanceof JLayeredPane) {
+				jlPane = (JLayeredPane)jrCom[0];
+			} else {
+				jlPane = (JLayeredPane)jrCom[1];
+			}
+			Component[] jlCom = jlPane.getComponents();
+			JMenuBar menubar;
+			if (jlCom[0] instanceof JMenuBar) {
+				menubar = (JMenuBar)jlCom[0];
+			} else {
+				menubar = (JMenuBar)jlCom[1];
+			}
+			JMenu menu = menubar.getMenu(0);
+			Component[] meCom = menu.getMenuComponents();
+			JMenuItem i = (JMenuItem)meCom[0];
+			ActionListener act = i.getActionListeners()[0];
+			act.actionPerformed(new ActionEvent(this, 0, "hello"));
+			assertFalse(this.m.isDisplayable());
+		} catch (@SuppressWarnings("unused") HeadlessException e) {
+			System.out.println("Test failed to run due to lack of monitor");
 		}
-		Component[] jlCom = jlPane.getComponents();
-		JMenuBar menubar;
-		if (jlCom[0] instanceof JMenuBar) {
-			menubar = (JMenuBar)jlCom[0];
-		} else {
-			menubar = (JMenuBar)jlCom[1];
-		}
-		JMenu menu = menubar.getMenu(0);
-		Component[] meCom = menu.getMenuComponents();
-		JMenuItem i = (JMenuItem)meCom[0];
-		ActionListener act = i.getActionListeners()[0];
-		act.actionPerformed(new ActionEvent(this, 0, "hello"));
-		assertFalse(this.m.isDisplayable());
 	}
 }
