6b6283e2a4c56e5a09f936a67fd4856f4b3c65be
hindlejosh <hindlejosh@ecs.vuw.ac.nz>
2020-10-02 11:37:31 +1300

    Add tests for the PostconditionViolationException class

--- src/test/java/test/nz/ac/vuw/engr301/group9mcs/commons/TestExceptions.java
+++ src/test/java/test/nz/ac/vuw/engr301/group9mcs/commons/TestExceptions.java
@@ -6,9 +6,11 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
-
 import nz.ac.vuw.engr301.group9mcs.commons.conditions.Null;
+import nz.ac.vuw.engr301.group9mcs.commons.conditions.PostconditionViolationException;
 import nz.ac.vuw.engr301.group9mcs.commons.conditions.PreconditionViolationException;
+import nz.ac.vuw.engr301.group9mcs.commons.conditions.InvariantViolationException;
+import nz.ac.vuw.engr301.group9mcs.commons.conditions.NOAAException;
 
 /**
  * Tests creating new InvariantViolation, PostconditionViolation,
@@ -43,6 +45,39 @@ public class TestExceptions {
 		assertNull(e.getCause().getMessage());
 
 		e = Null.nonNull(assertThrows(PreconditionViolationException.class, () -> {throw new PreconditionViolationException("err", new Throwable(), true, false);}));
+		assertEquals("err", e.getMessage());
+		assertNotNull(e.getCause());
+		assertNull(e.getCause().getMessage());
+		assertEquals(0, e.getStackTrace().length);
+	}
+
+	/**
+	 * Tests the PostconditionViolationException class.
+	 */
+	@SuppressWarnings("static-method")
+	@Test
+	public void testPostconditionViolationExceptions() {
+		PostconditionViolationException e;
+		e = Null.nonNull(assertThrows(PostconditionViolationException.class, () -> {throw new PostconditionViolationException();}));
+		assertNull(e.getMessage());
+		assertNull(e.getCause());
+
+		e = Null.nonNull(assertThrows(PostconditionViolationException.class, () -> {throw new PostconditionViolationException("err");}));
+		assertEquals("err", e.getMessage());
+		assertNull(e.getCause());
+
+		e = Null.nonNull(assertThrows(PostconditionViolationException.class, () -> {throw new PostconditionViolationException(new Throwable());}));
+		assertNotNull(e.getCause());
+		assertNull(e.getCause().getMessage());
+
+		e = Null.nonNull(assertThrows(PostconditionViolationException.class, () -> {
+			throw new PostconditionViolationException("err", new Throwable());}));
+		assertEquals("err", e.getMessage());
+		assertNotNull(e.getCause());
+		assertNull(e.getCause().getMessage());
+
+		e = Null.nonNull(assertThrows(PostconditionViolationException.class, () -> {
+			throw new PostconditionViolationException("err", new Throwable(), true, false);}));
 		assertEquals("err", e.getMessage());
 		assertNotNull(e.getCause());
 		assertNull(e.getCause().getMessage());
