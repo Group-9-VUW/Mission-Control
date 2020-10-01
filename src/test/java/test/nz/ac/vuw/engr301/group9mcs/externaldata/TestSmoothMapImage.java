e36cf24cd34a78891a105a556e48371c48cec536
hindlejosh <hindlejosh@vuw.ac.nz>
2020-10-02 11:54:59 +1300

    Change SmoothMapImageTest so it no longer fails occasionally

--- src/test/java/test/nz/ac/vuw/engr301/group9mcs/externaldata/TestSmoothMapImage.java
+++ src/test/java/test/nz/ac/vuw/engr301/group9mcs/externaldata/TestSmoothMapImage.java
@@ -14,7 +14,7 @@ import nz.ac.vuw.engr301.group9mcs.externaldata.map.SmoothMapImage;
 
 /**
  * Tests the SmoothMapImage class.
- * 
+ *
  * @author Joshua Hindley
  */
 public class TestSmoothMapImage {
@@ -28,13 +28,13 @@ public class TestSmoothMapImage {
 	public void testGetImage() {
 		try {
 			CachedMapImage cache = new CachedMapImage(new File(CachedMapImage.IMG_CACHE_FOLDER + "black-dot.png"));
-			SmoothMapImage map = new SmoothMapImage(cache, new SimpleEventListener() {			
+			SmoothMapImage map = new SmoothMapImage(cache, new SimpleEventListener() {
 				@Override
 				public void event(@NonNull String type) {/*This event does not need to be handled.*/}
 			});
-			BufferedImage img = (BufferedImage) map.get(-41.3, 174.75, -41.31, 174.76);
+			BufferedImage img = (BufferedImage) map.get(-41.3, 174.75, -41.307, 174.756);
 			assertNotEquals(img, cache.getImage());
-			BufferedImage img2 = (BufferedImage) map.get(-41.27, 174.72, -41.29, 174.74);
+			BufferedImage img2 = (BufferedImage) map.get(-41.307, 174.756, -41.31, 174.76);
 			assertNotEquals(img.getWidth(), img2.getWidth());
 			assertNotEquals(img.getHeight(), img2.getHeight());
 			assertNotEquals(img, img2);
