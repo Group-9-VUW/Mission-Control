7d1a6e35447594e0787ecd42845e86bab219a86e
jewellbail <jewellbail@ecs.vuw.ac.nz>
2020-10-06 12:25:00 +1300

    Added docs for LandingSitesData.

--- src/main/java/nz/ac/vuw/engr301/group9mcs/externaldata/map/LandingSitesData.java
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/externaldata/map/LandingSitesData.java
@@ -12,8 +12,17 @@ import java.util.List;
  * @author jewellbail
  */
 public class LandingSitesData {
+	/**
+	 * Point of rocket launch.
+	 */
     private final Point LAUNCH_SITE;
+    /**
+     * List of all possible landing sites.
+     */
     private final List<Point> LANDING_POINTS_ALL;
+    /**
+     * List of only valid landing sites.
+     */
     private final List<Point> LANDING_POINTS_VALID;
 
     /**
@@ -29,14 +38,23 @@ public class LandingSitesData {
         this.LANDING_POINTS_VALID = landingSitesValid;
     }
 
+    /**
+     * @return Returns the point of rocket launch.
+     */
     public Point getLaunchSite() {
         return this.LAUNCH_SITE;
     }
 
+    /**
+     * @return Returns an unmodifiable collection of all landing sites.
+     */
     public List<Point> getLandingPointsAll() {
         return Null.nonNull(Collections.unmodifiableList(this.LANDING_POINTS_ALL));
     }
 
+    /**
+     * @return Returns an unmodifiable collection of only valid landing sites.
+     */
     public List<Point> getLandingPointsValid() {
         return Null.nonNull(Collections.unmodifiableList(this.LANDING_POINTS_VALID));
     }
