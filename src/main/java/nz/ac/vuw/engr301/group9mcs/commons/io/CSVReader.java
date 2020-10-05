2c4ed0d53ecb71846326c99c7cd10685d786750e
hindlejosh <hindlejosh@ecs.vuw.ac.nz>
2020-10-06 10:42:08 +1300

    Fix bug and formatting

--- src/main/java/nz/ac/vuw/engr301/group9mcs/commons/io/CSVReader.java
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/commons/io/CSVReader.java
@@ -14,96 +14,95 @@ import nz.ac.vuw.engr301.group9mcs.commons.conditions.PreconditionViolationExcep
 
 /**
  * A reader for csv files
- * 
+ *
  * @author Claire
  */
 public class CSVReader implements Closeable {
-	
+
 	/**
 	 * The underlying reader for this reader
 	 */
 	private final BufferedReader reader;
-	
+
 	/**
 	 * An Iterator of all the lines in the file
 	 */
 	private final Iterator<String> lines;
-	
+
 	/**
 	 * The current line
 	 */
 	private String @Nullable[] currentLine;
-	
+
 	/**
 	 * The current value
 	 */
 	private int value = 0;
-	
+
 	/**
 	 * @param file The file to read from
 	 * @throws PreconditionViolationException if the file doesn't exist
 	 * @throws IOException if there's an error reading from the file
 	 */
 	@SuppressWarnings("null")
-	public CSVReader(File file) throws IOException
-	{
+	public CSVReader(File file) throws IOException {
 		if(!file.exists() || !file.isFile())
 			throw new PreconditionViolationException("File to read doesn't exist");
-		
+
 		this.reader = new BufferedReader(new FileReader(file));
 		this.lines = this.reader.lines().iterator();
 	}
-	
+
 	/**
 	 * @return Whether a next line is available
 	 */
-	public boolean hasNextLine()
-	{
+	public boolean hasNextLine() {
 		return this.lines.hasNext();
 	}
-	
+
 	/**
 	 * @return Whether a next value is available
 	 */
-	public boolean hasNextValue()
-	{
+	public boolean hasNextValue() {
 		if(this.currentLine == null)
 			return false;
 		return this.value < Null.nonNull(this.currentLine).length;
 	}
-	
+
 	/**
 	 * Moves this reader to the next line
 	 */
-	public void nextLine()
-	{
+	public void nextLine() {
 		String str = this.lines.next();
 		this.currentLine = str.split(",", count(str, ',') + (str.charAt(str.length() - 1) == ',' ? 0 : 1));
+		String last = Null.nonNull(this.currentLine)[Null.nonNull(this.currentLine).length - 1];
+		System.out.println(last);
+		if(last.endsWith(",")) {
+			Null.nonNull(this.currentLine)[Null.nonNull(this.currentLine).length - 1] = last.substring(0, last.length() - 1);
+		}
 		this.value = 0;
 	}
-	
+
 	/**
 	 * @return The next value on the current line
 	 */
-	public String nextValue()
-	{
+	public String nextValue() {
 		if(this.currentLine == null || !this.hasNextValue())
 			throw new PreconditionViolationException("No line in memory, or hasNextValue() is false");
 		return Null.nonNull(Null.nonNull(this.currentLine)[this.value++]);
 	}
-	
+
 	/**
 	 * @param str The string to check
 	 * @param c the character to count
 	 * @return The number of c instances in str
 	 */
-	public static int count(String str, char c)
-	{
+	public static int count(String str, char c) {
 		int lastindex = 0;
 		int count = 0;
 		while(lastindex != -1)  {
 			lastindex = str.indexOf(c, lastindex);
-			if(lastindex != -1) { 
+			if(lastindex != -1) {
 				count++;
 				lastindex++;
 			}
@@ -112,8 +111,7 @@ public class CSVReader implements Closeable {
 	}
 
 	@Override
-	public void close() throws IOException 
-	{
+	public void close() throws IOException {
 		this.reader.close();
 	}
 
