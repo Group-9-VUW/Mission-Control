195425eb7959285aff3cc53b1ab69798e0d38482
Claire Chambers <chambeconn@ecs.vuw.ac.nz>
2020-10-13 10:31:22 +1300

    Added saveLaunch()

--- src/main/java/nz/ac/vuw/engr301/group9mcs/controller/SavedLaunch.java
+++ src/main/java/nz/ac/vuw/engr301/group9mcs/controller/SavedLaunch.java
@@ -1,10 +1,21 @@
 package nz.ac.vuw.engr301.group9mcs.controller;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
 import java.util.List;
 
 import nz.ac.vuw.engr301.group9mcs.commons.LaunchRodData;
+import nz.ac.vuw.engr301.group9mcs.commons.map.PlanetaryArea;
+import nz.ac.vuw.engr301.group9mcs.commons.map.Point;
 import nz.ac.vuw.engr301.group9mcs.externaldata.map.CachedMapImage;
+import nz.ac.vuw.engr301.group9mcs.externaldata.map.CachedOsmOverpassData;
+import nz.ac.vuw.engr301.group9mcs.externaldata.map.InternetMapImage;
+import nz.ac.vuw.engr301.group9mcs.externaldata.map.OsmOverpassData;
+import nz.ac.vuw.engr301.group9mcs.externaldata.map.OsmOverpassGetter;
+import nz.ac.vuw.engr301.group9mcs.externaldata.weather.NOAA;
 import nz.ac.vuw.engr301.group9mcs.externaldata.weather.NOAAWeatherData;
 
 /**
@@ -82,6 +93,35 @@ public class SavedLaunch {
 	 */
 	public File getRocketData() {
 		return this.rocketData;
+	}
+	
+	/**
+	 * Saves a launch to file
+	 * 
+	 * @param rocket
+	 * @param rod
+	 * @param data
+	 * @param area 
+	 * 
+	 * @throws IOException 
+	 */
+	public static final void saveLaunch(File rocket, LaunchRodData rod, List<NOAAWeatherData> data, PlanetaryArea area) throws IOException
+	{
+		Files.copy(rocket.toPath(), new File(ROOT_DIR + "rocket.ork").toPath());
+		
+		File rodData = new File(ROOT_DIR + "rod.dat");
+		try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(rodData))) {
+			oos.writeObject(rodData);
+		}
+		
+		NOAA.writeToFile(new File(ROOT_DIR + "weather.dat"), data);
+		
+		OsmOverpassData osmData = OsmOverpassGetter.getAreasInBox(area.getUpperLeftLatitude(), area.getUpperLeftLongitude(), area.getBottomRightLatitude(), area.getBottomRightLongitude());
+		CachedOsmOverpassData.saveArea(new Point(area.getUpperLeftLatitude(), area.getUpperLeftLongitude()), new Point(area.getBottomRightLatitude(), area.getBottomRightLongitude()), osmData);
+		
+		File image = new File(ROOT_DIR + "image.png");
+		@SuppressWarnings("unused")
+		CachedMapImage ci = new CachedMapImage(new InternetMapImage(), area.getUpperLeftLatitude(), area.getUpperLeftLongitude(), area.getBottomRightLatitude(), area.getBottomRightLongitude(), image);
 	}
 
 }
